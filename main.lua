951717356498a1401a0fe040156a4b8287148a9a
Carter Hill <carterghill@gmail.com>
2018-01-27 16:03:31 -0600

    Added a circle and hit detection for it

--- main.lua
+++ main.lua
@@ -4,9 +4,20 @@ require "gooi"
 myLines = {}    -- List of all lines in the game
 circles = {}    -- List of all circles in the game
 
--- Add a new line to the game 
+-- Add a new line to the game
 function drawLine(x1, y1, x2, y2)
     myLines[#myLines+1] = {["x1"] = x1, ["y1"] = y1, ["x2"] = x2, ["y2"] = y2}
+end
+
+-- Add a circle to the game
+function createCircle(x, y, r)
+  circles[#circles+1] = {
+    ["x"] = x,
+    ["y"] = y,
+    ["r"] = r,
+    ["dx"] = 0,
+    ["dy"] = 0
+  }
 end
 
 -- Returns the (x, y) of the nearest point on a given line
@@ -26,12 +37,25 @@ function distance ( x1, y1, x2, y2 )
   return math.sqrt ( dx * dx + dy * dy )
 end
 
+-- Gets the distance from a point to a line
 function distFromLine(x, y, line)
-    
+
     x2, y2 = ClosestPointOnLineSegment(x, y, line["x1"], line["y1"], line["x2"], line["y2"])
-    
+
     return distance(x, y, x2, y2)
-    
+
+end
+
+-- Check if a circle is intersecting a line
+function circleOnLine(circle, line)
+
+  -- If the distance from the circles (x, y) to the line is less than its radius
+  if distFromLine(circle.x, circle.y, line) < circle.r then
+    return true
+  end
+
+  return false
+
 end
 
 -- The function called once at the very start
@@ -40,10 +64,10 @@ function love.load()
 
     -- Create a new button and store it in "exitButton"
 	exitButton = gooi.newButton({text = "Exit"})
-    
+
     -- button:onRelease() takes one function as an argument
     -- This function is run once the button is released
-    exitButton:onRelease( 
+    exitButton:onRelease(
     function()
         gooi.confirm({
             text = "Are you sure?",
@@ -52,31 +76,37 @@ function love.load()
             end
         })
 	end)
-    
+
     -- Draw line across bottom to use as the floor
     drawLine(-100, 600, 900, 600)
-    
+    createCircle(400, 0, 10)
+
 end
 
 function love.update()
     gooi.update()
+
 end
 
 function love.draw()
 
     -- Draw all Gooi elements
 	gooi.draw()
-    
+
     -- Draw all lines
     for i=1, #myLines do
-    
+
         love.graphics.line(myLines[i]["x1"], myLines[i]["y1"],
                             myLines[i]["x2"], myLines[i]["y2"])
     end
-    
+
+    for i=1, #circles do
+      love.graphics.circle( "fill", circles[i].x, circles[i].y, circles[i].r )
+    end
+
     x, y = love.mouse.getPosition()
     love.graphics.print(distFromLine(x, y, myLines[1]), 10, 100)
-    
+
 end
 
 function love.mousepressed(x, y, button)
